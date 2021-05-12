a8e49202e2ee122f4168a659194a64569655b13a
olokken <ole.lokken1@gmail.com>
2021-05-12 10:53:39 +0200

    ja

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,22 +16,13 @@ export const UserContext = createContext<any>(null);
 function App() {
   const [user, setUser] = useState<string>('');
   const value = useMemo(() => ({ user, setUser }), [user, setUser]);
-  const link = from([new HttpLink({ uri: 'http://localhost:4000/graphql' })]);
 
-  const client = new ApolloClient({
-    cache: new InMemoryCache({
-      //addTypename: false,
-    }),
-    link: link,
-  });
   return (
-    <ApolloProvider client={client}>
-      <UserContext.Provider value={value}>
-        <BrowserRouter>
-          <div className="App">{Routes}</div>
-        </BrowserRouter>
-      </UserContext.Provider>
-    </ApolloProvider>
+    <UserContext.Provider value={value}>
+      <BrowserRouter>
+        <div className="App">{Routes}</div>
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
--- client/src/interfaces/Price.tsx
+++
@@ -1,5 +0,0 @@
-export default interface Price {
-  __typename: string;
-  sum: number;
-  date: Date;
-}
