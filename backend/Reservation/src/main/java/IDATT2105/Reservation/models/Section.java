b687975238f2911e07fb0c32b941fc8c6c148fdb
haavarty <haavarty@stud.ntnu.no>
2021-05-16 16:39:54 +0200

    Added some checks in creating reservation

--- backend/Reservation/src/main/java/IDATT2105/Reservation/models/Section.java
+++ backend/Reservation/src/main/java/IDATT2105/Reservation/models/Section.java
@@ -76,6 +76,14 @@ public class Section {
     this.capacity = capacity;
   }
 
+  public List<Reservation> getReservations() {
+    return reservations;
+  }
+
+  public void setReservations(List<Reservation> reservations) {
+    this.reservations = reservations;
+  }
+
   public void removeReservation(Reservation reservation) {
     for(int i = 0; i < this.reservations.size(); i++) {
       if(this.reservations.get(i).getReservation_id() == reservation.getReservation_id()) {
